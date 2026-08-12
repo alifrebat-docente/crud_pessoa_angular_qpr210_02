b429e8cb64caca1be270620986c3da89cd3979ca
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-11 21:02:01 -0300

    Alterado a classe Pessoa

--- src/app/models/pessoa.ts
+++ src/app/models/pessoa.ts
@@ -1,12 +1,9 @@
-import { UF } from "./uf"
-import { Municipio } from "./municipio"
-
 export class Pessoa {
     id? : number
     nome?: string
     email?: string
     cpf? : string
     dataNascimento?: string
-    uf?: UF
-    municipio?: Municipio
+    uf : string = ''
+    municipio : string = ''
 }
